d02695ac1153f73a8fe7063474c16c0480766872
Rahul Rathod <rahulrathod6203@gmail.com>
2023-07-11 12:32:59 +0530

    Pattern class modified

--- src/com/cg/patterns/Patterns.java
+++ src/com/cg/patterns/Patterns.java
@@ -1,20 +1,19 @@
 package com.cg.patterns;
 
 public class Patterns {
-	
-	
+
 	static void pattern1(int n) {
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<=n;j++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= n; j++) {
 				System.out.print("* ");
 			}
 			System.out.println();
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 
+		// TODO Auto-generated method stub
 		pattern1(5);
 
 	}
