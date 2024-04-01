382a2e7af6d681179453c4257f9e6988f050fcf8
Rahul Rathod <rahulrathod6203@gmail.com>
2024-04-01 15:16:00 +0530

    Data Types

---
+++ Test/src/DataTypes.java
@@ -0,0 +1,28 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        //Primitive - byte,short,int,long,float,double,boolean and char
+        //Non-Primitive - String, Arrays and Classes
+
+        byte byteNumber=29;
+
+        short shortNumber=19999;
+
+        int integerNumber=1999999999;
+
+        long longNumber=1999999999;
+
+        float floatNumber=12.2f;
+
+        double doubleNumber=199.3;
+
+        char character='c';
+
+        boolean booleanValue=false;
+
+        System.out.println(floatNumber);
+
+
+
+    }
+}
